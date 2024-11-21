11c2079c12c8c4da380d1bcce0e5b4c9bf6d2ca8
iku701 <iku701@naver.com>
2024-11-21 12:26:35 +0900

    now commit

--- src/main/java/edu/du/myproject1101_1/controller/CustomErrorController.java
+++ src/main/java/edu/du/myproject1101_1/controller/CustomErrorController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
+    
+    //통합 에러페이지
     @RequestMapping("/error")
     public String handleError() {
         return "view/error/error"; // templates/view/error/error.html 경로
